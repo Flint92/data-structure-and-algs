10ce2ccec6de97cbebf77f0421043903c1d86c78
Flint92 <1992flint@gmail.com>
2024-05-05 18:28:14 +0800

    -- prim

---
+++ algs/graph/prim/prrm_test.go
@@ -0,0 +1,31 @@
+package prim
+
+import (
+	"github.com/stretchr/testify/require"
+	"graph"
+	"testing"
+)
+
+func TestPrim(t *testing.T) {
+
+	g := graph.NewGraph()
+	g.AddEdgeUndirected("A", "B", 6)
+	g.AddEdgeUndirected("A", "C", 1)
+	g.AddEdgeUndirected("A", "D", 5)
+	g.AddEdgeUndirected("B", "C", 5)
+	g.AddEdgeUndirected("B", "E", 3)
+	g.AddEdgeUndirected("C", "D", 5)
+	g.AddEdgeUndirected("C", "E", 6)
+	g.AddEdgeUndirected("C", "F", 4)
+	g.AddEdgeUndirected("D", "F", 2)
+	g.AddEdgeUndirected("E", "F", 6)
+
+	edges := MST(g)
+	require.Equal(t, 5, len(edges))
+	require.Equal(t, &Edge{src: "A", dst: "C", weight: 1.0}, edges[0])
+	require.Equal(t, &Edge{src: "C", dst: "F", weight: 4.0}, edges[1])
+	require.Equal(t, &Edge{src: "F", dst: "D", weight: 2.0}, edges[2])
+	require.Equal(t, &Edge{src: "C", dst: "B", weight: 5.0}, edges[3])
+	require.Equal(t, &Edge{src: "B", dst: "E", weight: 3.0}, edges[4])
+
+}
