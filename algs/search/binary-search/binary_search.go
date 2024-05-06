ed63667af6f8f725eea099fa0cb6405930aa0c32
Flint92 <1992flint@gmail.com>
2024-05-07 00:40:34 +0800

    -- binary search

--- algs/search/binary-search/binary_search.go
+++ algs/search/binary-search/binary_search.go
@@ -1,12 +1,16 @@
 package binary_search
 
-func BinarySearch(nums []int, item int) int {
+func BinarySearch(nums []int, target int) int {
 	left, right := 0, len(nums)-1
+	if target < nums[left] || target > nums[right] {
+		return -1
+	}
+
 	for left <= right {
 		mid := left + (right-left)>>1
-		if item == nums[mid] {
+		if target == nums[mid] {
 			return mid
-		} else if item < nums[mid] {
+		} else if target < nums[mid] {
 			right = mid - 1
 		} else {
 			left = mid + 1
--- algs/search/binary-search/binary_search_test.go
+++ algs/search/binary-search/binary_search_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21}
 
 	require.Equal(t, BinarySearch(nums, 1), 0)
 	require.Equal(t, BinarySearch(nums, 10), 9)
-	require.Equal(t, BinarySearch(nums, 20), 19)
-	require.Equal(t, BinarySearch(nums, 21), -1)
+	require.Equal(t, BinarySearch(nums, 20), 18)
+	require.Equal(t, BinarySearch(nums, 17), -1)
+	require.Equal(t, BinarySearch(nums, 0), -1)
+	require.Equal(t, BinarySearch(nums, 22), -1)
 
 }
